800c00a339acd8317c7742f2c4049cf6df048124
stevegdavis <stevedavis@post.com>
2023-02-23 08:32:20 +0000

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace POIViewerMap;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace POIViewerMap;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
