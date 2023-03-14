60778c66d46c5fd86570c61409fb6d667b573650
stevegdavis <stevedavis@post.com>
2023-03-14 08:57:16 +0000

    Hidden max distance hardcoded to 10km

--- Views/MapViewPage.xaml.cs
+++ Views/MapViewPage.xaml.cs
@@ -30,7 +30,7 @@ public partial class MapViewPage : ContentPage
     static string bicyclerepairstationStr = null;
     static bool POIsReadIsBusy = false;
     static bool POIsMapUpdateIsBusy = false;
-    static int MaxDistancePOIShow = 10; //Meters
+    static int MaxDistancePOIShow = 10; //Km
     static int MinZoomPOI = 40;
     private List<POIData> pois = new();
     private static Location myCurrentLocation;
