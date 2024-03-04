78eafce78eb4a382d74be6f54443d02d98d38be0
stevegdavis <stevedavis@post.com>
2024-03-04 13:55:40 +0000

    Update WebHelper.cs

--- Helpers/WebHelper.cs
+++ Helpers/WebHelper.cs
@@ -12,7 +12,7 @@ namespace POIViewerMap.Helpers;
 
 class WebHelper
 {
-    private static readonly string ServerUrl = "http://192.168.1.182:4000/poidata-server";
+    private static readonly string ServerUrl = "https://sdsdevelopment.w5.lt/poidata-server";// "http://192.168.1.182:4000/poidata-server";
     private static readonly string Download = "uploads/france.bin";
     private static readonly string FILES = "client/index.php";
     public static readonly string PARAM_ACTION = "action";
